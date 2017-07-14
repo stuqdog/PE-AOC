4dffcc221b43f80f67e6d7b19ff6a3eae926e01e
Ethan <ethanrodkin@gmail.com>
2017-07-14 18:13:45 -0400

    Added notes to explain what's going on.

--- aoc4.py
+++ aoc4.py
@@ -18,7 +18,7 @@ import string
 
 letters = string.ascii_letters
 numbers = string.digits
-first_strip = "%s[]\n%s" % (letters, numbers)
+first_strip = "%s[]\n" % letters
 all_but_numbers = "%s[]-\n" % letters
 
 solution = 0
@@ -28,15 +28,30 @@ script, text = argv
 with open(text) as f:
     for line in f:
         match = True
+
+        # tracking the five most common letters
         char_one, char_two, char_three, = None, None, None
         char_four, char_five = None, None
+
+        # tracking the # of occurrences of char_one through char_five
         char_one_num, char_two_num, char_three_num = 0, 0, 0
         char_four_num, char_five_num = 0, 0
+
+        # strips \n from lines so we have a consistent placement of the 5 letter
+        # key's placement relative to the end of the line
         line_s = line.rstrip("\n")
+
+        # creates our key
         key = [line_s[-6], line_s[-5], line_s[-4], line_s[-3], line_s[-2]]
+
+        #removes key from the line so it doesn't get counted in our test.
         sort_line = line.rstrip(first_strip)
+
+        # pulls out the room's sector ID.
         room_value = int(line.strip(all_but_numbers))
-    #reads sorted characters, keeps track of five biggest occurrences
+
+    #reads sorted characters, looks for occurrences of all characters
+    #in the alphabet. Keeps track of five most frequent occurrences.
         for char in letters:
             if sort_line.count(char) > char_one_num:
                 char_five_num, char_five = char_four_num, char_four
@@ -59,8 +74,10 @@ with open(text) as f:
             elif sort_line.count(char) > char_five_num:
                 char_five_num, char_five = sort_line.count(char), char
 
+        #the five most common letters, in order
         test = [char_one, char_two, char_three, char_four, char_five]
-        print key, test
+
+        # checks to see that test == key. If it is, add room_value to solution.
         for x in range(0, 5):
             if key[x] != test[x]:
                 match = False
