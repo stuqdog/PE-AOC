550338bc53ce8093824142834528fc42962697ac
Ethan <ethanrodkin@gmail.com>
2017-07-13 22:10:36 -0400

    updated urban(), it can now generate rolls in die ranges up to xxdyyy.

--- ex36.py
+++ ex36.py
@@ -81,12 +81,29 @@ def urban():
     with open(urban_table) as f:
         for x in range(1, roll + 1):
             result = f.readline()
-            die_num = int(result[0])
-            die_size = int(result[2])
+
+
+    die_num = " "#int(result[0])
+    die_size = " "#int(result[2])
+    for x in range(0, 2):
+        if result[x] in string.digits:
+            die_num += result[x]
+        else:
+            die_num = die_num.lstrip()
+            break
+    die_num = die_num.lstrip()
+    find_size = result.lstrip("0123456789")
+    find_size = find_size.lstrip("d")
+    for x in range(0, 3):
+        if find_size[x] in string.digits:
+            die_size += find_size[x]
+        else:
+            die_size = die_size.lstrip()
+            break
 
     encounter_number = 0
-    for r in range(0, die_num):
-        encounter_number += random.randint(1, die_size)
+    for r in range(0, int(die_num)):
+        encounter_number += random.randint(1, int(die_size))
 
     print encounter_number, result.lstrip("1234567890d ")
     urban_end()
