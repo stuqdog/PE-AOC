22130ac39d6ed7de604c6b78f3633aafbeedaec9
Ethan <ethanrodkin@gmail.com>
2017-07-14 17:28:32 -0400

    Made new list of steps, testing them.

--- aoc4.py
+++ aoc4.py
@@ -1,4 +1,10 @@
-#1. set loop to repeat a number of times equal to the # of lines. DONE
+#1. set loop to repeat for each line. DONE
+#2. Read the line, remove 5 letter key at the end and the room code. DONE
+#3. Sort line. DONE
+#4. Remove '-' from line.
+#5. Sort line by most common occurrence, then delete duplicates.
+#6. Compare sorted line to 5 letter key.
+#7 If sorted line and 5 letter key match, add room code to sum.
 #2 read the line, parsing at -. add letters before numbers to check set,
 #  add letter after numbers to key set. Add letters to check_sum set.
 #3 sort check set by most common appearance, remove duplicates.
@@ -6,20 +12,20 @@
 #  add check_sum to solution.
 
 
-#How about this?
-
-
 
 from sys import argv
+import string
+
+letters = string.ascii_letters
+numbers = string.digits
+first_strip = "%s[]\n" % letters
+
 
 script, text = argv
 
 with open(text) as f:
-    loop_number = len(f.readlines())
-
-with open(text) as f:
-    for x in range(0, 3):
-        loop = f.readline()
-        #loop_parsed = loop[0]
-        print loop
-        #print loop_parsed
+    for line in f:
+        sort_line = line.rstrip(first_strip)
+        sort_line = sort_line.rstrip(numbers)
+        sort_line = sorted(sort_line)
+        print sort_line
