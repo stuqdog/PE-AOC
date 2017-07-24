005e6dde1c30393870701e46d356ce3b50db0225
Ethan <ethanrodkin@gmail.com>
2017-07-23 20:08:26 -0400

    aoc8b, not working. Can't modify string values one char at a time.

---
+++ aoc8b.py
@@ -0,0 +1,52 @@
+# read the directions, break them up by text.
+# once we know right direction, reduce it to the numbers necessary
+# for rotate, grid[x][y] = grid_template[x - rotate][y]
+
+
+from sys import argv
+import string
+
+script, text = argv
+
+numbers = string.digits
+numbers += "\n"
+
+grid = ["_", "_", "_", "_", "_", "_"]
+
+## grid[a][b] would be the a row, and b column
+
+for y in range(0, 6):
+    for x in range(0, 49):
+        grid[y] += "_"
+
+with open(text) as f:
+    for line in f:
+        grid_record = grid
+        if "rect" in line:
+            rect_size = line.lstrip("rect ")
+            rect_x = rect_size.rstrip(numbers)
+            rect_x = int(rect_x.rstrip("x"))
+            rect_y = rect_size.lstrip(numbers)
+            rect_y = rect_y.rstrip("\n")
+            rect_y = int(rect_y.lstrip("x"))
+            for x in range(0, rect_x):
+                for y in range(0, rect_y):
+                    grid[x][y] = "X"
+        elif "row" in line:
+            rotate_x = line.lstrip("rotate row y=")
+            row = rotate_x.rstrip(numbers)
+            row = int(row.rstrip(" by "))
+            shift = rotate_x.lstrip(numbers)
+            shift = int(shift.lstrip(" by "))
+            for x in range(0, 50):
+                grid[row][x] = grid_record[row][(x + 50 - shift) % 50]
+        elif "column" in line:
+            rotate_y = line.lstrip("rotate row x=")
+            column = rotate_y.rstrip(numbers)
+            column = int(column.rstrip(" by "))
+            shift = rotate_y.lstrip(numbers)
+            shift = int(shift.lstrip(" by "))
+            for x in range(0, 6):
+                grid[x][column] = grid_record[column][(x + 6 - shift) % 6]
+
+print grid[0], grid[1], grid[2], grid[3], grid[4], grid[5]
