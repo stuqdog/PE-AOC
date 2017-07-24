0d63f9733444888d0bcf9687c2c69dfd9d3531f5
Ethan <ethanrodkin@gmail.com>
2017-07-23 20:48:20 -0400

    Solved earlier issue with list of lists. but it keeps resetting...

--- aoc8b.py
+++ aoc8b.py
@@ -11,16 +11,26 @@ script, text = argv
 numbers = string.digits
 numbers += "\n"
 
-grid = ["_", "_", "_", "_", "_", "_"]
+row_zero = []
+row_one = []
+row_two = []
+row_three = []
+row_four = []
+row_five = []
+
+grid = [row_zero, row_one, row_two, row_three, row_four, row_five]
+grid_record = grid
 
 ## grid[a][b] would be the a row, and b column
 
 for y in range(0, 6):
-    for x in range(0, 49):
-        grid[y] += "_"
+    for x in range(0, 50):
+        grid[y].append("_")
+
 
 with open(text) as f:
     for line in f:
+        print grid
         grid_record = grid
         if "rect" in line:
             rect_size = line.lstrip("rect ")
@@ -29,24 +39,22 @@ with open(text) as f:
             rect_y = rect_size.lstrip(numbers)
             rect_y = rect_y.rstrip("\n")
             rect_y = int(rect_y.lstrip("x"))
-            for x in range(0, rect_x):
-                for y in range(0, rect_y):
-                    grid[x][y] = "X"
-        elif "row" in line:
+            for y in range(0, rect_y):
+                for x in range(0, rect_x):
+                    grid[y][x] = "X"
+        elif "rotate row" in line:
             rotate_x = line.lstrip("rotate row y=")
             row = rotate_x.rstrip(numbers)
             row = int(row.rstrip(" by "))
             shift = rotate_x.lstrip(numbers)
             shift = int(shift.lstrip(" by "))
             for x in range(0, 50):
-                grid[row][x] = grid_record[row][(x + 50 - shift) % 50]
-        elif "column" in line:
-            rotate_y = line.lstrip("rotate row x=")
+                grid[row][x] = grid_record[row][((x + 50) - shift) % 50]
+        elif "rotate column" in line:
+            rotate_y = line.lstrip("rotate column x=")
             column = rotate_y.rstrip(numbers)
             column = int(column.rstrip(" by "))
             shift = rotate_y.lstrip(numbers)
             shift = int(shift.lstrip(" by "))
             for x in range(0, 6):
-                grid[x][column] = grid_record[column][(x + 6 - shift) % 6]
-
-print grid[0], grid[1], grid[2], grid[3], grid[4], grid[5]
+                grid[x][column] = grid_record[(x + 6 - shift) % 6][column]
