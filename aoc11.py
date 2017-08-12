fddcced02448c847f12868a31a37b3f5b38feece
Ethan <ethanrodkin@gmail.com>
2017-08-11 21:47:03 -0400

    A lot faster. Trick was making prev_positions vars strs, not objects.

--- aoc11.py
+++ aoc11.py
@@ -46,22 +46,42 @@ previous_positions = []
 
 
 class Map(object):
-
-    def __init__(self, h_chip, h_gen, l_chip, l_gen, elevator):
-        self.h_chip = h_chip
-        self.h_gen = h_gen
-        self.l_chip = l_chip
-        self.l_gen = l_gen
-        self.item_list = [self.h_chip, self.h_gen, self.l_chip, self.l_gen]
+## variables: th, pl, st, pr, ru
+    def __init__(self, th_chip, th_gen, pl_chip, pl_gen, st_chip,
+                 st_gen, pr_chip, pr_gen, ru_chip, ru_gen, elevator):
+        self.th_chip = th_chip
+        self.th_gen = th_gen
+        self.pl_chip = pl_chip
+        self.pl_gen = pl_gen
+        self.st_chip = st_chip
+        self.st_gen = st_gen
+        self.pr_chip = pr_chip
+        self.pr_gen = pr_gen
+        self.ru_chip = ru_chip
+        self.ru_gen = ru_gen
+        self.item_list = [self.th_chip, self.th_gen, self.pl_chip, self.pl_gen,
+                          self.st_chip, self.st_gen, self.pr_chip, self.pr_gen,
+                          self.ru_chip, self.ru_gen]
         self.elevator = elevator
 
 
 
     def legality_check(self):
         """This checks to see if a state is legal, and returns False if not"""
-        if (self.h_chip != self.h_gen) and (self.h_chip == self.l_gen):
+        if (self.th_chip != self.th_gen) and (self.th_chip in [
+              self.pl_gen, self.st_gen, self.pr_gen, self.ru_gen]):
             return False
-        elif (self.l_chip != self.l_gen) and (self.l_chip == self.h_gen):
+        elif (self.pl_chip != self.pl_gen) and (self.pl_chip in [
+              self.th_gen, self.st_gen, self.pr_gen, self.ru_gen]):
+            return False
+        elif (self.st_chip != self.st_gen) and (self.st_chip in [
+              self.th_gen, self.pl_gen, self.pr_gen, self.ru_gen]):
+            return False
+        elif (self.pr_chip != self.pr_gen) and (self.pr_chip in [
+              self.th_gen, self.st_gen, self.pl_gen, self.ru_gen]):
+            return False
+        elif (self.ru_chip != self.ru_gen) and (self.ru_chip in [
+              self.th_gen, self.st_gen, self.pr_gen, self.pl_gen]):
             return False
         else:
             return True
@@ -70,27 +90,39 @@ class Map(object):
 
 
 
-current_move_positions = [Map(1, 2, 1, 3, 1)]
+current_move_positions = [Map(1, 1, 2, 1, 2, 1, 3, 3, 3, 3, 1)]
 
-while step_number < 15:
+
+while True:
     # all this is creating objects of potential future positions
     print step_number
     legal_next_steps = []
-    for position in current_move_positions:
 
-        if (position.h_chip == 4 and position.h_gen == 4 and
-        position.l_chip == 4 and position.l_gen == 4):
+    for position in current_move_positions:
+        success = True
+        for item in position.item_list:
+            if item != 4:
+                success = False
+                break
+        if success == True:
             print step_number
             exit()
 
 
         this_map_layout = [
-            position.h_chip,
-            position.h_gen,
-            position.l_chip,
-            position.l_gen
+            position.th_chip,
+            position.th_gen,
+            position.pl_chip,
+            position.pl_gen,
+            position.st_chip,
+            position.st_gen,
+            position.pr_chip,
+            position.pr_gen,
+            position.ru_chip,
+            position.ru_gen
         ]
-        object_names = ['h_chip', 'h_gen', 'l_chip', 'l_gen']
+        object_names = ['th_chip', 'th_gen', 'pl_chip', 'pl_gen', 'st_chip',
+                        'st_gen', 'pr_chip', 'pr_gen', 'ru_chip', 'ru_gen']
 
         movable_object_list = []
         duo_object_temp = []
@@ -109,44 +141,67 @@ while step_number < 15:
         for item in duo_object_temp:
             movable_object_list.append(item)
 
-        for item in movable_object_list:
-            if position.elevator == 4:
-                pass
-            else:
-                new_layout = [position.h_chip, position.h_gen,
-                              position.l_chip, position.l_gen]
+
+        if position.elevator == 4:
+            pass
+        else:
+            for item in movable_object_list:
+
+                new_layout = [position.th_chip, position.th_gen,
+                    position.pl_chip, position.pl_gen, position.st_chip,
+                    position.st_gen, position.pr_chip, position.pr_gen,
+                    position.ru_chip, position.ru_gen]
 
                 for x in xrange(0, len(object_names)):
                     if object_names[x] in item:
                         new_layout[x] += 1
 
-                test = Map(new_layout[0], new_layout[1],
-                    new_layout[2], new_layout[3], position.elevator + 1)
+                test = Map(new_layout[0], new_layout[1], new_layout[2],
+                    new_layout[3], new_layout[4], new_layout[5], new_layout[6],
+                    new_layout[7], new_layout[8], new_layout[9],
+                    position.elevator + 1)
                 legal = test.legality_check()
-                if test not in previous_positions and legal == True:
+                repeat_test = ''
+                for item in test.item_list:
+                    repeat_test += str(item)
+                if repeat_test in previous_positions:
+                    legal = False
+                # if test not in previous_positions and legal == True:
+                if legal == True:
                     legal_next_steps.append(test)
 
-        for item in movable_object_list:
-            if position.elevator == 1:
-                pass
-            else:
-                new_layout = [position.h_chip, position.h_gen,
-                              position.l_chip, position.l_gen]
+        if position.elevator == 1:
+            pass
+        else:
+            for item in movable_object_list:
+                new_layout = [position.th_chip, position.th_gen,
+                    position.pl_chip, position.pl_gen, position.st_chip,
+                    position.st_gen, position.pr_chip, position.pr_gen,
+                    position.ru_chip, position.ru_gen]
 
                 for x in xrange(0, len(object_names)):
                     if object_names[x] in item:
                         new_layout[x] -= 1
 
-                test = Map(new_layout[0], new_layout[1],
-                    new_layout[2], new_layout[3], position.elevator - 1)
+                test = Map(new_layout[0], new_layout[1], new_layout[2],
+                    new_layout[3], new_layout[4], new_layout[5], new_layout[6],
+                    new_layout[7], new_layout[8], new_layout[9],
+                    position.elevator - 1)
                 legal = test.legality_check()
-                if test not in previous_positions and legal == True:
+                repeat_test = ''
+                for item in test.item_list:
+                    repeat_test += str(item)
+                if repeat_test in previous_positions:
+                    legal = False
+                if legal == True:
+                # if legal == True:
                     legal_next_steps.append(test)
 
 
-
-    previous_positions.append(position)
-
+        prev_position = ''
+        for item in position.item_list:
+            prev_position += str(item)
+        previous_positions.append(prev_position)
     current_move_positions = legal_next_steps
     step_number += 1
 
