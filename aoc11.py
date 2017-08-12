f7c0bb0c00e4cc99087567f78cc7c75d8a4a20c1
Ethan <ethanrodkin@gmail.com>
2017-08-12 11:45:12 -0400

    Removed Map class, also added a note about efficiently eliminating duos.

--- aoc11.py
+++ aoc11.py
@@ -1,45 +1,33 @@
 from sys import exit
 
 
-class Map(object):
-
-    def __init__(self, item_floor):
-## item_floor vars: see object_names below. also: elevator at [-1] position.
-        self.item_floor = item_floor
-
-
-    def legality_check(self):
-        """This checks to see if a state is legal, and returns False if not"""
-        if (self.item_floor[0] != self.item_floor[1]) and (self.item_floor[0]
-            in [self.item_floor[3], self.item_floor[5], self.item_floor[7],
-            self.item_floor[9]]):
-            return False
-        elif (self.item_floor[2] != self.item_floor[3]) and (self.item_floor[2]
-            in [self.item_floor[1], self.item_floor[5], self.item_floor[7],
-            self.item_floor[9]]):
-            return False
-        elif (self.item_floor[4] != self.item_floor[5]) and (self.item_floor[5]
-            in [self.item_floor[3], self.item_floor[1], self.item_floor[7],
-            self.item_floor[9]]):
-            return False
-        elif (self.item_floor[6] != self.item_floor[7]) and (self.item_floor[6]
-            in [self.item_floor[3], self.item_floor[5], self.item_floor[1],
-            self.item_floor[9]]):
-            return False
-        elif (self.item_floor[8] != self.item_floor[9]) and (self.item_floor[8]
-            in [self.item_floor[3], self.item_floor[5], self.item_floor[7],
-            self.item_floor[1]]):
-            return False
-        else:
-            return True
+def legality_check(item_floor):
+    """This checks to see if a state is legal, and returns False if not"""
+    if (item_floor[0] != item_floor[1]) and (item_floor[0]
+        in [item_floor[3], item_floor[5], item_floor[7], item_floor[9]]):
+        return False
+    elif (item_floor[2] != item_floor[3]) and (item_floor[2]
+        in [item_floor[1], item_floor[5], item_floor[7], item_floor[9]]):
+        return False
+    elif (item_floor[4] != item_floor[5]) and (item_floor[5]
+        in [item_floor[3], item_floor[1], item_floor[7], item_floor[9]]):
+        return False
+    elif (item_floor[6] != item_floor[7]) and (item_floor[6]
+        in [item_floor[3], item_floor[5], item_floor[1], item_floor[9]]):
+        return False
+    elif (item_floor[8] != item_floor[9]) and (item_floor[8]
+        in [item_floor[3], item_floor[5], item_floor[7], item_floor[1]]):
+        return False
+    else:
+        return True
 
 # starting_positions and object_names entries correlate to one another.
 
 object_names = ['th_chip', 'th_gen', 'pl_chip', 'pl_gen', 'st_chip',
                 'st_gen', 'pr_chip', 'pr_gen', 'ru_chip', 'ru_gen']
 starting_positions = [1, 1, 2, 1, 2, 1, 3, 3, 3, 3, 1]
-current_move_positions = [Map(starting_positions)]
-previous_positions = [Map(starting_positions)]
+current_move_positions = [starting_positions]
+previous_positions = [starting_positions]
 step_number = 0
 previous_positions = []
 
@@ -51,7 +39,7 @@ while True:
     for position in current_move_positions:
         success = True
         # check to see if any given position is a winner.
-        for item in position.item_floor:
+        for item in position:
             if item != 4:
                 success = False
                 break
@@ -64,9 +52,12 @@ while True:
         duo_object_temp_storage = []
 
         for x in xrange(0, 10):
-            if position.item_floor[x] == position.item_floor[10]:
+            if position[x] == position[10]:
                 movable_object_list.append(object_names[x])
 
+# Note to self: we can probably make some slight efficiency gains here.
+# if a duo consists of a chip and a non-matching gen, we know right away it's
+# not legal.
         for x in xrange(0, len(movable_object_list)):
             for y in xrange(x + 1, len(movable_object_list)):
                 duo = movable_object_list[x] + " " + movable_object_list[y]
@@ -77,11 +68,11 @@ while True:
 
 
 # Legal moves going up. If elevator == 4 then we're already at the top.
-        if position.item_floor[10] < 4:
+        if position[10] < 4:
             for item in movable_object_list:
 
                 new_layout = []
-                for i in position.item_floor:
+                for i in position:
                     new_layout.append(i)
 
                 for x in xrange(0, len(object_names)):
@@ -89,22 +80,21 @@ while True:
                         new_layout[x] += 1
 
                 new_layout[10] += 1
-                test = Map(new_layout)
-                legal = test.legality_check()
+                legal = legality_check(new_layout)
 
                 if legal == False:
                     pass
                 elif new_layout not in previous_positions:
-                    legal_next_steps.append(test)
+                    legal_next_steps.append(new_layout)
                     previous_positions.append(new_layout)
 
 
 # Legal moves going down. If elevator == 1 then we're already at bottom floor.
-        if position.item_floor[10] > 1:
+        if position[10] > 1:
             for item in movable_object_list:
 
                 new_layout = []
-                for i in position.item_floor:
+                for i in position:
                     new_layout.append(i)
 
                 for x in xrange(0, len(object_names)):
@@ -112,12 +102,11 @@ while True:
                         new_layout[x] -= 1
 
                 new_layout[10] -= 1
-                test = Map(new_layout)
-                legal = test.legality_check()
+                legal = legality_check(new_layout)
                 if legal == False:
                     pass
                 elif new_layout not in previous_positions:
-                    legal_next_steps.append(test)
+                    legal_next_steps.append(new_layout)
                     previous_positions.append(new_layout)
 
 
