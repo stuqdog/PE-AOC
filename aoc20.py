328a49c48ec63623711a8b8519a4dc48d0578a99
Ethan <ethanrodkin@gmail.com>
2017-09-02 01:23:31 -0400

    Got right answer but not entirely accurate. Notes on the problem within.

--- aoc20.py
+++ aoc20.py
@@ -10,6 +10,9 @@ with open("aoc20.txt") as f:
 
 ip_list = sorted(ip_list, key=lambda ip_range: ip_range[0])
 
+#note: this gets the right answer in this case, but won't always. IP ranges can
+#overlap, so we could be in the range of ip_list[i-1] but not of [i] or [i+1]
+
 for i in xrange(0, list_size):
     x = ip_list[i][1] + 1
     if x < ip_list[i + 1][0]:
