386547d8495733fdbc75f223f2364015564eacb0
Ethan <ethanrodkin@gmail.com>
2017-12-12 14:13:16 -0500

    Cleaned, new inputs

--- AOC2.py
+++ AOC2.py
@@ -1,68 +1,27 @@
-from sys import argv
-
-script, readf = argv
-
-digit = 5
+with open('aoc2.txt') as f:
+    instructions = [line.strip() for line in f]
 
 
-with open(readf) as f:
-    while True:
-        input = f.read(1)
-        if not input:
-            break
-        if input == "U" and digit > 3:
-            digit -= 3
-        elif input == "D" and digit < 7:
-            digit += 3
-        elif input == "L" and digit not in [1, 4, 7]:
-            digit -= 1
-        elif input == "R" and digit not in [3, 6, 9]:
-            digit += 1
-        elif input == "V":
-            print digit
+rule = {'U': (0, -1), 'L': (-1, 0), 'R': (1, 0), 'D': (0, 1)}
+x, y = 1, 1
+solution = ''
+# Part one:
+# layout = [[1, 2, 3],
+#           [4, 5, 6],
+#           [7, 8, 9]]
 
+# part two:
+layout = [[0, 0, 1, 0, 0],
+          [0, 2, 3, 4, 0],
+          [5, 6, 7, 8, 9],
+          [0, 'A', 'B', 'C', 0],
+          [0, 0, 'D', 0, 0]]
 
-
-
-
-
-
-
-
-
-
-
-
-def formula(digit, input, r_f, digit_num):
-    with open(r_f) as f:
-        while True:
-            input = f.read(1)
-            while digit_num < 5:
-                if input == "U":
-                    if digit > 3:
-                        digit -= 3
-            #    else:
-            #        digit = digit
-                elif input == "D":
-                    if digit < 7:
-                        digit += 3
-                #else: digit = digit
-                elif input == "L":
-                    if digit != 1 and digit != 4 and digit != 7:
-                        digit -= 1
-                #else:
-                #    digit = digit
-                elif input == "R":
-                    if digit != 3 and digit != 6 and digit != 9:
-                        digit += 1
-                #else:
-                #    digit = digit
-                elif input == "V":
-                    digit_num += 1
-                    print "Digit %d: %d" % digit_num, digit
-            if not input:
-                break
-
-
-#with open(readf) as f:
-#formula(5, "U", readf, 0)
+for i in instructions:
+    for c in i:
+        # For part one, change range to 3
+        if x + rule[c][0] in range(5) and y + rule[c][1] in range(5) and (
+                layout[y + rule[c][1]][x + rule[c][0]] != 0):
+            x, y = x + rule[c][0], y + rule[c][1]
+    solution += str(layout[y][x])
+print(solution)
